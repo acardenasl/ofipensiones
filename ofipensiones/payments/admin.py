6b4985a899c7df1f57761548647340716ce02fb6
Isaac David <idbl64@outlook.com>
2024-10-18 22:44:47 -0500

    Add Bill to admin

--- ofipensiones/payments/admin.py
+++ ofipensiones/payments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import (
+    Bill,
     Grade,
     GradeTerm,
     Institution,
@@ -14,4 +15,5 @@ admin.site.register(Grade)
 admin.site.register(GradeTerm)
 admin.site.register(Student)
 admin.site.register(TermPayment)
+admin.site.register(Bill)
 admin.site.register(OfipensionesLog)
