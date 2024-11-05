08b168a8cb8247c3b5d7c667c4d23885800d706c
acardenasl-uniandes <142956045+acardenasl-uniandes@users.noreply.github.com>
2024-11-05 12:52:54 -0500

    locustfile.py

--- test/load/locustfile.py
+++ test/load/locustfile.py
@@ -1,13 +1,23 @@
-from locust import HttpUser, tag, task
+from locust import HttpUser, task, between, tag
 
+# Usuario con comportamiento normal: menos de 100 peticiones por minuto
+class NormalUser(HttpUser):
+    wait_time = between(0.6, 0.7)  # Aproximadamente 100 peticiones por minuto
 
-class LoadPaymentReceiveService(HttpUser):
     @tag("payments")
     @task
-    def payment_receipt(self):
+    def normal_behavior_payment_receipt(self):
         self.client.get("/api/payment_receipts/")
 
     @tag("bills")
     @task
-    def bills(self):
+    def normal_behavior_bills(self):
         self.client.get("/api/bills/")
+
+# Usuario con comportamiento malicioso: más de 100 peticiones por minuto
+class MaliciousUser(HttpUser):
+    wait_time = between(0.1, 0.2)  # Mucho más de 100 peticiones por minuto
+
+    @task
+    def malicious_behavior(self):
+        self.client.get("/api/payment_receipts/")
